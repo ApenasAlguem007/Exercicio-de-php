e2ab5678b068e8b50a4e5abfff614e29fd650c34
GEOVANNA PEREIRA DA SILVA <geovanna.psilva9@senacsp.edu.br>
2025-01-31 09:00:16 -0300

    atualização

---
+++ m001/saudação-com-nome.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ptbr
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saudação com Nome </title>
+</head>
+<body>
+    
+<h2> Bom dia! </h2>
+    
+<?php
+
+// atribui um dado a uma variavel 
+
+$nome = "Samuel";
+$sobrenome = "Santos Rodrigues";
+
+const pesquisa = "Temperatura local ";
+
+echo "Olá, $nome  $sobrenome, seja Bem-vindo ao nosso Site! <br></br>";
+echo "Sua pesquisa anterior foi referente à " . pesquisa . "deseja retomá-la?"
+
+?>
+
+
+</h2>
+
+</body>
+</html>
