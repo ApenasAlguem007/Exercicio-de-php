faec422967bcf3f1340b3f9ae148e92fa88b7f19
GEOVANNA PEREIRA DA SILVA <geovanna.psilva9@senacsp.edu.br>
2025-02-03 11:15:26 -0300

    exercicio-novo

--- ex010/index.php
+++ ex010/index.php
@@ -13,7 +13,36 @@
 
 // Array é uma lista de variaveis de diversos tipos.
 
+// criaçao de array indexado
 
+$array = [10, 20, 30, 40];
+
+// exibindo o array de forma legivel
+
+echo "Array original:";
+print_r ($array);
+
+echo "<br>";
+
+// adicionando um valor oa final da array
+
+$array[] = 50;
+
+// exibindo o array atualizado 
+
+echo " Array após adicionar o 50";
+
+print_r($array);
+
+echo "<br>";
+
+// mudando o valor da pposiçao 1 para 25
+
+$array[1] = 25; 
+
+echo "Array após alterar o valor na posição 1 para 25: ";
+
+print_r($array);
 
 ?>
 
